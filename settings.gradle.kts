2ef55d23fbbe1b5bc98fe5bafe50be4146c408d6
Cyrillo Praditya Soeharto <cyrillo.praditya41@ui.ac.id>
2026-02-19 13:28:00 +0700

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "MySawit-Manajemen_Kebun_Sawit"
---
+++ src/main/java/id/ac/ui/cs/advprog/mysawitmanajemen_kebun_sawit/MySawitManajemenKebunSawitApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.mysawitmanajemen_kebun_sawit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySawitManajemenKebunSawitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySawitManajemenKebunSawitApplication.class, args);
+    }
+
+}
