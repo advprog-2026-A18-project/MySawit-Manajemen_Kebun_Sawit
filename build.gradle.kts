9c457b051c0e773c8e29f126e5c5a4302c310de0
Cyrillo Praditya Soeharto <cyrillo.praditya41@ui.ac.id>
2026-06-07 12:46:39 +0700

    add securityconfig

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,7 @@ repositories {
 
 dependencies {
     // implementation
+    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("me.paulschwarz:spring-dotenv:$dotenvVersion")
     implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
